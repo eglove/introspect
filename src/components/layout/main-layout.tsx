bc451281bff718d24fbc79c69ec67c368a1bd5cc
Ethan Glover <hello@ethang.email>
2024-02-18 11:06:22 -0600

    move to component

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -6,31 +6,32 @@ import globalCss from '../../global.css';
 
 export const mainLayout = createRootRoute({
   component() {
-    return (
-      <html lang="en-US">
-        <head>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>Introspect</title>
-          <style>{globalCss as string}</style>
-        </head>
-        <body>
-          <div className="flex gap-2 p-2">
-            <Link to="/" className="[&.active]:font-bold">
-              Home
-            </Link>{' '}
-            <Link to="/about" className="[&.active]:font-bold">
-              About
-            </Link>
-          </div>
-          <hr />
-          <Outlet />
-          <TanStackRouterDevtools />
-        </body>
-      </html>
-    );
+    return MainLayout;
   },
 });
+
+function MainLayout() {
+  return (
+    <html lang="en-US">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Introspect</title>
+        <style>{globalCss as string}</style>
+      </head>
+      <body>
+        <div className="flex gap-2 p-2">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>{' '}
+          <Link to="/about" className="[&.active]:font-bold">
+            About
+          </Link>
+        </div>
+        <hr />
+        <Outlet />
+        <TanStackRouterDevtools />
+      </body>
+    </html>
+  );
+}
